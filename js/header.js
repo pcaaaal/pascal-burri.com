c50fef8f986ca54e4b01b4b7f560682e95a00f9e
pascal <pascal_burri@gmx.ch>
2024-03-26 09:41:04 +0100

    create start page

--- js/header.js
+++ js/header.js
@@ -17,3 +17,30 @@ document.addEventListener('mousemove', function(e) {
         header.style.transform = 'translateY(-60px)';
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.glitch-word').forEach(link => {
+        link.innerHTML = link.textContent.split('').map(letter => `<span class="glitch-letter">${letter}</span>`).join('');
+    });
+
+    document.querySelectorAll('.glitch-word').forEach(word => {
+        word.addEventListener('mouseenter', () => {
+            word.querySelectorAll('.glitch-letter').forEach(letter => {
+                let randomDelay = Math.random() * 0.5;
+                let randomDuration = Math.random() * 1;
+                letter.style.animation = `glitch ${randomDuration}s ${randomDelay}s both`;
+
+                letter.addEventListener('animationend', () => {
+                    letter.style.animation = `underline infinite 0.8s both`;
+                });
+            });
+        });
+
+        word.addEventListener('mouseleave', () => {
+            word.querySelectorAll('.glitch-letter').forEach(letter => {
+                letter.style.animation = 'none';
+            });
+        });
+    });
+});
