fbb1c5986af75acc08b50d18e188c847b04443fc
Pascal <pascal_burri@gmx.ch>
2024-04-10 21:46:28 +0200

    add some more overflow hidden

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '@/styles/globals.css';
 import '@/styles/blob.css';
+import '@/styles/globals.css';
 import '@/styles/header.css';
 import type {AppProps} from 'next/app';
-import Header from '../components/header';
 import {useState} from 'react';
 import Blob from '../components/blob';
+import Header from '../components/header';
 
 export default function App({Component, pageProps}: AppProps) {
 	const [dark, setDark] = useState(false);
@@ -12,12 +12,12 @@ export default function App({Component, pageProps}: AppProps) {
 		<div
 			className={`tw-w-full tw-h-full tw-flex tw-flex-col tw-items-center tw-bg-white ${dark ? 'tw-dark' : ''} tw-overflow-hidden`}
 		>
-			<Blob>
-				<div className="tw-relative tw-overflow-hidden">
+			<div className="tw-overflow-hidden tw-w-full tw-h-full">
+				<Blob>
 					<Header setDark={setDark} dark={dark} />
 					<Component {...pageProps} />
-				</div>
-			</Blob>
+				</Blob>
+			</div>
 		</div>
 	);
 }
