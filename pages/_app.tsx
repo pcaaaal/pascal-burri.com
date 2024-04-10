9b8841248b8535231b4a99643bcc81cca1982fad
Pascal <pascal_burri@gmx.ch>
2024-04-10 20:00:37 +0200

    added overflow-hideden

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,10 +10,10 @@ export default function App({Component, pageProps}: AppProps) {
 	const [dark, setDark] = useState(false);
 	return (
 		<div
-			className={`tw-w-[100vw] tw-h-[100vh] tw-flex tw-flex-col tw-items-center tw-bg-white ${dark ? 'tw-dark' : ''} `}
+			className={`tw-w-[100vw] tw-h-[100vh] tw-flex tw-flex-col tw-items-center tw-bg-white ${dark ? 'tw-dark' : ''} tw-overflow-hidden`}
 		>
 			<Blob>
-				<div className="tw-relative">
+				<div className="tw-relative tw-overflow-hidden">
 					<Header setDark={setDark} dark={dark} />
 					<Component {...pageProps} />
 				</div>
