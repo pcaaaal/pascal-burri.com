0ab4b2602dd24be5dfd6b26416fdadbebb3feeff
pascal <pascal_burri@gmx.ch>
2024-07-02 09:14:41 +0200

    maybe fixed theme flickering. test in pushed env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,34 @@ import type {AppProps} from 'next/app';
 import {useEffect, useState} from 'react';
 import Header from '../components/Site/Header';
 import Background from '../components/Background/Background';
+
 export default function App({Component, pageProps}: AppProps) {
 	const [dark, setDark] = useState(true);
+	const [themeLoaded, setThemeLoaded] = useState(false);
 
 	useEffect(() => {
-		if (window.localStorage.getItem('theme') === null) {
+		const storedTheme = window.localStorage.getItem('theme');
+		if (!storedTheme) {
 			window.localStorage.setItem('theme', 'dark');
 		}
-		window.localStorage.getItem('theme') === 'dark'
-			? setDark(true)
-			: setDark(false);
+		setDark(storedTheme === 'dark');
+		setThemeLoaded(true);
 	}, []);
+
+	if (!themeLoaded) {
+		return null; // or a loading spinner if you prefer
+	}
 
 	return (
 		<div
-			className={`tw-w-full tw-h-full tw-flex tw-flex-col tw-items-center tw-bg-white ${dark ? 'tw-dark' : ''} tw-overflow-hidden tw-overflow-y-hidden tw-overflow-x-hidden dark:tw-text-white tw-text-black`}
+			className={`tw-w-full tw-h-full tw-flex tw-flex-col tw-items-center tw-bg-white ${
+				dark ? 'tw-dark' : ''
+			} tw-overflow-hidden dark:tw-text-white tw-text-black`}
 		>
 			<div className="tw-w-full tw-h-full tw-overflow-hidden">
 				<Background>
 					<Header setDark={setDark} dark={dark} />
-					<div className=" tw-justify-center tw-items-center">
+					<div className="tw-justify-center tw-items-center">
 						{/* <Modal /> */}
 					</div>
 					<div className="tw-z-20">
