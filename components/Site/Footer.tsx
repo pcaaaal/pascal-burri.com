d645e07e26a776dd6ba43908e89d5d5379f3fefc
“Pascal Burri” <pasca.burri2@swisscom.com>
2024-10-17 09:18:10 +0200

    resolve some z index buggs

--- components/Site/Footer.tsx
+++ components/Site/Footer.tsx
@@ -1,20 +1,30 @@
 const Footer = () => {
 	return (
-		<div className="tw-p-6 tw-sm:p-12 tw-z-20 tw-flex tw-w-full tw-justify-center tw-items-center">
-			<span className="tw-text-center">
-				Website made by{' '}
-				<a
-					href="https://pascal-burri.com/"
-					target="_blank"
-					className="font-semibold hover:text-white"
-					rel="noreferrer"
-				>
-					Pascal Burri
-				</a>
-				, made with NextJS, and Tailwind © {new Date().getFullYear()} All
-				rights reserved. Illustrations by{' '}
-				<a href="https://www.freepik.com/" rel="noreferrer" target="_blank">freepik</a>.
-			</span>
+		<div className="tw-p-6 tw-sm:p-12 tw-flex tw-w-full tw-justify-center tw-items-center">
+			<div className="tw-z-20 tw-block">
+				<span className="tw-text-center">
+					Website made by{' '}
+					<a
+						href="https://pascal-burri.com/"
+						target="_blank"
+						className="font-semibold hover:text-white"
+						rel="noreferrer"
+					>
+						Pascal Burri
+					</a>
+					, made with NextJS, and Tailwind ©{' '}
+					{new Date().getFullYear()} All rights reserved.
+					Illustrations by{' '}
+					<a
+						href="https://www.freepik.com/"
+						rel="noreferrer"
+						target="_blank"
+					>
+						freepik
+					</a>
+					.
+				</span>
+			</div>
 		</div>
 	);
 };
